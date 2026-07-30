019100e891aa301c66f97fa89f113fde1380ecc5
Raafat Turki <raafat.turki@proton.me>
2026-07-30 16:18:21 +0300

    chore: add raafat.io as a preview allowed host

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,18 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	build: {
-		assetsInlineLimit: Infinity
-	}
+  plugins: [sveltekit()],
+  build: {
+    assetsInlineLimit: Infinity
+  },
+  server: {
+    host: "0.0.0.0",
+    allowedHosts: ["raafat.io"],
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: 4173,
+    strictPort: true,
+    allowedHosts: ["raafat.io"],
+  },
 });
